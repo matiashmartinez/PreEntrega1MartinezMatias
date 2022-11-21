0f3d104d90ec1d251a7dec9704bf3a4f98ae6823
Matías Hernán Martinez <mati8910@hotmail.com>
2022-11-20 23:33:15 -0300

    ok

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import '../index.css'
+
+const Footer = () => {
+  return (
+    <>
+    <footer className="footer items-center p-4 bg-neutral ">
+        <div className='text-center  text-sm text-slate-200' >
+            <p>Todos los derechos reservados</p>
+        </div>
+    </footer>
+    </>
+    
+  );
+};
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import NavBar from "./NavBar";
+
+function Header() {
+  return (
+    
+    <NavBar />
+  )
+}
+export default Header
